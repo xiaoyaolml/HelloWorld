69b47c529698c013e919b019e380d34ae146c45e
Leon <xiaoyaolml@gmail.com>
2016-01-22 00:00:12 +0800

    修改一个文件，新增两个文件

--- app/src/main/java/com/leon/helloworld/MainActivity.java
+++ app/src/main/java/com/leon/helloworld/MainActivity.java
@@ -23,14 +23,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
-
-
-
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/leon/helloworld/TestNoAddClass.java
@@ -0,0 +1,7 @@
+package com.leon.helloworld;
+
+/**
+ * Created by LML on 2016-01-21.
+ */
+public class TestNoAddClass {
+}
